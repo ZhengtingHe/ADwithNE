3300b1f4f15442521e49f4123f9bc28b79981093
Zhengting He <hezhen@ethz.ch>
2024-04-22 21:41:28 +0200

    EMD_pot functions

---
+++ emd.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy.spatial.distance import cdist
+from ot.lp import emd_c, check_result
+
+def process_event(event):
+        # Sort constituents in event by pT
+        event = event[event[:, 0].argsort()]
+        pts = event[:, 0]
+        coords = event[:, 1:]
+        return pts, coords
+
+def emt_pot(source_event, target_event, R=1.0, return_flow=False, n_iter_max=100000):
+    # Compute energy  mover's distance between two events using python ot library 
+    
+    source_pts, source_coords = process_event(source_event)
+    target_pts, target_coords = process_event(target_event)
+
+    # Psuedorapidity-azimuth distance matrix
+    dist_matrix = cdist(source_coords, target_coords, 'euclidean')
+
+    _, cost, flow, _, result_code = emd_c(source_pts, target_pts, dist_matrix, numItermax=n_iter_max, log=True)
+    check_result(result_code)
+
+    if return_flow:
+        return cost, flow
+    else:
+        return cost
+
+
+    
+
