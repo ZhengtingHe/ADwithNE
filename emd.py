c27a5f1dae63ce488c89ce262a9cce0645e6ce45
Zhengting He <hezhen@ethz.ch>
2024-04-23 12:11:33 +0200

    Refactor EMD_pot function

--- emd.py
+++ emd.py
@@ -2,30 +2,64 @@ import numpy as np
 from scipy.spatial.distance import cdist
 from ot.lp import emd_c, check_result
 
+# Ref: https://github.com/SangeonPark/ToyJetGenerator/blob/main/optimal_transport/emd.py
+
 def process_event(event):
-        # Sort constituents in event by pT
         event = np.array(event)
-        event = event[event[:, 0].argsort()]
+        # Sort constituents in event by pT in descending order
+        # event = event[np.argsort(event[:, 0])[::-1]]
         pts = event[:, 0]
         coords = event[:, 1:]
-        return pts, coords
+        return np.ascontiguousarray(pts), np.ascontiguousarray(coords) # Return as contiguous arrays for C compatibility
 
 def check_shape(event):
-    assert (len(event.shape)) == 2 and (event.shape[-1] == 3) # Event shape must be (n, 3)
+    assert event.ndim == 2 and event.shape[-1] == 3, "Event shape must be (n, 3)"
 
-def emt_pot(source_event, target_event, R=1.0, return_flow=False, n_iter_max=100000):
+def emd_pot(source_event, target_event, norm=False, R=1.0, return_flow=False, n_iter_max=100000):
     # Compute energy  mover's distance between two events using python ot library 
     
     check_shape(source_event)
     check_shape(target_event)
 
-    source_pts, source_coords = process_event(source_event)
-    target_pts, target_coords = process_event(target_event)
+    source_pTs, source_coords = process_event(source_event)
+    target_pTs, target_coords = process_event(target_event)
+    source_total_pT, target_total_pT = source_pTs.sum(), target_pTs.sum()
+    source_coords = source_coords
+    target_coords = target_coords
 
-    # Psuedorapidity-azimuth distance matrix
-    dist_matrix = cdist(source_coords, target_coords, 'euclidean')
+    # if norm, then we normalize the pts to 1
+    if norm:
+        source_pTs /= source_total_pT
+        target_pTs /= target_total_pT
+        thetas = cdist(source_coords, target_coords)/R
+        rescale = 1.0
 
-    _, cost, flow, _, result_code = emd_c(source_pts, target_pts, dist_matrix, numItermax=n_iter_max, log=True)
+    # implement the EMD in Eq. 1 of the paper by adding an appropriate extra particle
+    else:
+        pTdiff = target_total_pT - source_total_pT
+        if pTdiff > 0:
+            source_pTs = np.hstack((source_pTs, pTdiff))
+            coords0_extra = np.vstack((source_coords, np.zeros(source_coords.shape[1], dtype=np.float64)))
+            thetas = cdist(coords0_extra, target_coords)/R
+            thetas[-1,:] = 1.0
+
+        elif pTdiff < 0:
+            target_pTs = np.hstack((target_pTs, -pTdiff))
+            coords1_extra = np.vstack((target_coords, np.zeros(target_coords.shape[1], dtype=np.float64)))
+            thetas = cdist(source_coords, coords1_extra)/R
+            thetas[:,-1] = 1.0
+
+        # in this case, the pts were exactly equal already so no need to add a particle
+        else:
+            thetas = cdist(source_coords, target_coords)/R
+
+        # change units for numerical stability
+        rescale = max(source_total_pT, target_total_pT)
+
+    # # Psuedorapidity-azimuth distance matrix
+    # dist_matrix = cdist(source_coords, target_coords, 'euclidean')
+
+    _, cost, flow, _, result_code = emd_c(source_pTs/rescale, target_pTs/rescale, thetas, n_iter_max, True)
     check_result(result_code)
 
     if return_flow:
