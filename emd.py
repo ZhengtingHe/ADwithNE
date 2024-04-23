c570acf08210e409a395efb23c52cfca658607b5
Zhengting He <hezhen@ethz.ch>
2024-04-24 01:37:48 +0200

    Visualization of OT

--- emd.py
+++ emd.py
@@ -2,22 +2,25 @@ import numpy as np
 from scipy.spatial.distance import cdist
 from ot.lp import emd_c, check_result
 
+
 # Ref: https://github.com/SangeonPark/ToyJetGenerator/blob/main/optimal_transport/emd.py
 
 def process_event(event):
-        event = np.array(event)
-        # Sort constituents in event by pT in descending order
-        # event = event[np.argsort(event[:, 0])[::-1]]
-        pts = event[:, 0]
-        coords = event[:, 1:]
-        return np.ascontiguousarray(pts), np.ascontiguousarray(coords) # Return as contiguous arrays for C compatibility
+    event = np.array(event)
+    # Sort constituents in event by pT in descending order
+    # event = event[np.argsort(event[:, 0])[::-1]]
+    pts = event[:, 0]
+    coords = event[:, 1:3]
+    return np.ascontiguousarray(pts), np.ascontiguousarray(coords)  # Return as contiguous arrays for C compatibility
+
 
 def check_shape(event):
-    assert event.ndim == 2 and event.shape[-1] == 3, "Event shape must be (n, 3)"
+    assert event.ndim == 2 and event.shape[-1] == 4, "Event shape must be (n, 4)"
+
 
 def emd_pot(source_event, target_event, norm=False, R=1.0, return_flow=False, n_iter_max=100000):
     # Compute energy  mover's distance between two events using python ot library 
-    
+
     check_shape(source_event)
     check_shape(target_event)
 
@@ -31,7 +34,7 @@ def emd_pot(source_event, target_event, norm=False, R=1.0, return_flow=False, n_
     if norm:
         source_pTs /= source_total_pT
         target_pTs /= target_total_pT
-        thetas = cdist(source_coords, target_coords)/R
+        thetas = cdist(source_coords, target_coords) / R
         rescale = 1.0
 
     # implement the EMD in Eq. 1 of the paper by adding an appropriate extra particle
@@ -39,19 +42,19 @@ def emd_pot(source_event, target_event, norm=False, R=1.0, return_flow=False, n_
         pTdiff = target_total_pT - source_total_pT
         if pTdiff > 0:
             source_pTs = np.hstack((source_pTs, pTdiff))
-            coords0_extra = np.vstack((source_coords, np.zeros(source_coords.shape[1], dtype=np.float64)))
-            thetas = cdist(coords0_extra, target_coords)/R
-            thetas[-1,:] = 1.0
+            source_coords_extra = np.vstack((source_coords, np.zeros(source_coords.shape[1], dtype=np.float64)))
+            thetas = cdist(source_coords_extra, target_coords) / R
+            thetas[-1, :] = 1.0
 
         elif pTdiff < 0:
             target_pTs = np.hstack((target_pTs, -pTdiff))
-            coords1_extra = np.vstack((target_coords, np.zeros(target_coords.shape[1], dtype=np.float64)))
-            thetas = cdist(source_coords, coords1_extra)/R
-            thetas[:,-1] = 1.0
+            target_coords_extra = np.vstack((target_coords, np.zeros(target_coords.shape[1], dtype=np.float64)))
+            thetas = cdist(source_coords, target_coords_extra) / R
+            thetas[:, -1] = 1.0
 
         # in this case, the pts were exactly equal already so no need to add a particle
         else:
-            thetas = cdist(source_coords, target_coords)/R
+            thetas = cdist(source_coords, target_coords) / R
 
         # change units for numerical stability
         rescale = max(source_total_pT, target_total_pT)
@@ -59,14 +62,10 @@ def emd_pot(source_event, target_event, norm=False, R=1.0, return_flow=False, n_
     # # Psuedorapidity-azimuth distance matrix
     # dist_matrix = cdist(source_coords, target_coords, 'euclidean')
 
-    flow_matrix, cost, _, _, result_code = emd_c(source_pTs/rescale, target_pTs/rescale, thetas, n_iter_max, True)
+    flow_matrix, cost, _, _, result_code = emd_c(source_pTs / rescale, target_pTs / rescale, thetas, n_iter_max, True)
     check_result(result_code)
 
     if return_flow:
         return cost * rescale, flow_matrix * rescale
     else:
         return cost
-
-
-    
-
--- visualize.py
+++ visualize.py
@@ -1,34 +1,104 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 from utils import parse_event
 
+marker_scale = 30
 
-def plot_event_cloud(event):
-    met_part, electron_part, muon_part, jet_part = parse_event(event)
 
+def create_eta_phi_plane():
+    # Create a 2D eta-phi plane
     plt.figure(figsize=(5, 5))
     plt.xlim(-5, 5)
     plt.ylim(-3.5, 3.5)
     plt.xlabel(r'$\eta$')
     plt.ylabel(r'$\phi$')
-
-    plt.scatter(met_part[:, 1], met_part[:, 2], color='red', marker='^', s=met_part[:, 0], label='MET')
-
-    if electron_part.size:
-        plt.scatter(electron_part[:, 1], electron_part[:, 2], color='blue', marker='o', s=electron_part[:, 0],
-                    label='Electrons')
-
-    if muon_part.size:
-        plt.scatter(muon_part[:, 1], muon_part[:, 2], color='green', marker='s', s=muon_part[:, 0], label='Muons')
-
-    if jet_part.size:
-        plt.scatter(jet_part[:, 1], jet_part[:, 2], color='purple', marker='*', s=jet_part[:, 0], label='Jets')
-
-    plt.legend()
     plt.grid(True)
+
+
+def create_legent():
+    legend = plt.legend(loc="lower left", scatterpoints=1, fontsize=10)
+    for handle in legend.legend_handles:
+        handle.set_sizes([30.0])
+
+
+def plot_constituents(event, state='source'):
+    particle_colors = np.array([
+        ["#89CFF0", "#007BFF"],
+        ["#DA70D6", "#800080"],
+        ["#FFA07A", "#FF4500"],
+        ["#D3D3D3", "#708090"]
+    ])
+
+    color_state = 1 if state == 'target' else 0
+    plot_colors = particle_colors[:, color_state]
+
+    met_part, electron_part, muon_part, jet_part = parse_event(event)
+
+    particle_parts = [met_part, electron_part, muon_part, jet_part]
+    markers = ['o', '^', 's', 'D']
+    labels = ['MET', 'Electrons', 'Muons', 'Jets']
+
+    for i, part in enumerate(particle_parts):
+        if part.size:
+            plt.scatter(
+                part[:, 1], part[:, 2],
+                color=plot_colors[i],
+                marker=markers[i],
+                s=part[:, 0] * marker_scale,
+                alpha=0.5,
+                label=state + labels[i]
+            )
+
+            for eta, phi, pt in zip(part[:, 1], part[:, 2], part[:, 0]):
+                plt.text(eta, phi, "{:.2f}".format(pt))
+
+
+
+
+def plot_event_cloud(event):
+    # Plot single event in the eta-phi plane
+    create_eta_phi_plane()
+    plot_constituents(event, state='')
+    create_legent()
     plt.show()
+
+
+def plot_optimal_transport(source_event, target_event, flow_matrix, emd):
+    create_eta_phi_plane()
+    # Plot source and target distributions
+    plot_constituents(source_event, state='source')
+    plot_constituents(target_event, state='target')
+    # Get coordinates and pTs of source and target events
+    source_pts = source_event[:, 0]
+    target_pts = target_event[:, 0]
+    source_coords = source_event[:, 1:3]
+    target_coords = target_event[:, 1:3]
+    # Create extra particles if needed
+    source_total_pt = source_pts.sum()
+    target_total_pt = target_pts.sum()
+    pt_diff = target_total_pt - source_total_pt
+    if pt_diff > 0:
+        source_coords = np.vstack((source_coords, np.zeros(source_coords.shape[1], dtype=np.float64)))
+        plt.scatter(0, 0, color="#CCFF00", marker='P', s=pt_diff * marker_scale, alpha=0.5, label='extra_source')
+    elif pt_diff < 0:
+        target_coords = np.vstack((target_coords, np.zeros(target_coords.shape[1], dtype=np.float64)))
+        plt.scatter(0, 0, color="#006400", marker='P', s=-pt_diff * marker_scale, alpha=0.5, label='extra_target')
+    # Take only non-zero flow values
+    index_send, index_receive = np.nonzero(flow_matrix)
+    # Plot arrows between source and target particles
+    sending_coords = source_coords[index_send]
+    receiving_coords = target_coords[index_receive]
+    for receiving_coord, sending_coord in zip(receiving_coords, sending_coords):
+        print("From: ", sending_coord, "To: ", receiving_coord)
+        plt.arrow(sending_coord[0], sending_coord[1],  # arrow base
+                  receiving_coord[0] - sending_coord[0], receiving_coord[1] - sending_coord[1],
+                  head_width=0.1, fc='#FFD700', ec='#FFD700')  # arrow span
+
+    create_legent()
+    plt.title(f'EMD: {emd:.2f}')
+    plt.show()
+
 
 def plot_hists(events, title):
     # Plot histograms of pT, eta and phi for MET, electrons, muons and jets
@@ -40,20 +110,20 @@ def plot_hists(events, title):
     colors = ['#8ECFC9', '#FFBE7A', '#FA7F6F']
 
     fig, axes = plt.subplots(nrows=4, ncols=3, figsize=(15, 15))
+
     for i in range(4):
         for j in range(3):
             axes[i, j].hist(particles[i][:, j], bins=50, color=colors[j])
             if j == 0:
-                axes[i, j].set_yscale('log') #Log scale for pT
+                axes[i, j].set_yscale('log')  # Log scale for pT
             axes[i, j].grid(True)
 
     for ax, col in zip(axes[0], variables):
         ax.set_title(col)
 
-    for ax, row in zip(axes[:,0], particle_names):
+    for ax, row in zip(axes[:, 0], particle_names):
         ax.set_ylabel(row, rotation=0)
 
-    
     fig.suptitle(title)
     fig.tight_layout()
     plt.show()
