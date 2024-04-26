f847133cc446743c343a52498eac9c4bf8cdba05
Zhengting He <hezhen@ethz.ch>
2024-04-26 21:37:08 +0200

    Dataset; EMD loaded from stored files

--- data_loader.py
+++ data_loader.py
@@ -4,6 +4,9 @@ import h5py
 import os
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
+
+from emd import emd_pot
 
 
 def load_config():
@@ -27,6 +30,47 @@ def get_h5_files():
     return config['bkg_files'], config['sig_files']
 
 
-def read_h5_file(path, file_name):
-    return np.array(h5py.File(os.path.join(path, file_name), 'r')['Particles'])
+def read_h5_file(path, file_name, datatype='Particles'):
+    if datatype == 'Particles':
+        return np.array(h5py.File(os.path.join(path, file_name), 'r')[datatype])
+    elif datatype == "EMD":
+        data = h5py.File(os.path.join(get_database_path(), "generated_data", file_name), "r")
+        return np.array(data["pairs"]), np.array(data["emds"])
+
+
+
+
+def sample_pairs(n_events, n_pairs):
+    # np.random.choice can be extremely slow, use randint instead
+    pairs = np.random.randint(0, n_events, (n_pairs, 2))
+    # remove pairs with same index since we don't want to compare the same event
+    bad_samples_idx = np.where(pairs[:, 0] == pairs[:, 1])
+    for i in bad_samples_idx:
+        pairs[i] = np.random.choice(n_events, 2, replace=False)
+    return pairs
+
+
+def sample_matrix(n_events, pairs):
+    matrix = np.zeros((n_events, n_events))
+    for pair in pairs:
+        matrix[pair[0], pair[1]] += 1
+    return matrix
+
+
+def sample_pairs_with_emd(events, n_pairs=None):
+    n_events = len(events)
+    if n_pairs is None:
+        n_pairs = 5 * n_events
+    pairs = sample_pairs(n_events, n_pairs)
+    emds = np.zeros(n_pairs)
+    for i, pair in enumerate(tqdm(pairs)):
+        emds[i] = emd_pot(events[pair[0]], events[pair[1]])
+    return pairs, emds
+
+
+def store_emds_with_pairs(emds, pairs, file_name):
+    f = h5py.File(os.path.join(get_database_path(), "generated_data", file_name), "w")
+    f["pairs"] = pairs
+    f["emds"] = emds
+    f.close()
 
